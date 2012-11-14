42e937e06933182ab502a830ed5e72a8022c6917
Alexei Mozhaev <lxmzhv@gmail.com>
2012-11-14 22:47:19 +0400

    Used photo_match lib in video_processor

--- matcher/matcher.cpp
+++ matcher/matcher.cpp
@@ -1,74 +1,123 @@
+/**
+ * @file SURF_Homography
+ * @brief SURF detector + descriptor + FLANN Matcher + FindHomography
+ * @author A. Huaman
+ */
+
 #include <stdio.h>
-#include <algorithm>
-
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
-
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/nonfree/features2d.hpp"
 
 using namespace cv;
 
-static void help()
+void readme();
+
+/**
+ * @function main
+ * @brief Main function
+ */
+int main( int argc, char** argv )
 {
-    printf("\nThis program demonstrates using features2d detector, descriptor extractor and simple matcher\n"
-            "Using the SURF desriptor:\n"
-            "\n"
-            "Usage:\n matcher_simple <image1> <image2>\n");
+  if( argc != 3 )
+  { readme(); return -1; }
+
+  Mat img_object = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
+  Mat img_scene = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+
+  if( !img_object.data || !img_scene.data )
+  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+
+  //-- Step 1: Detect the keypoints using SURF Detector
+  int minHessian = 400;
+
+  SurfFeatureDetector detector( minHessian );
+
+  std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+  detector.detect( img_object, keypoints_object );
+  detector.detect( img_scene, keypoints_scene );
+
+  //-- Step 2: Calculate descriptors (feature vectors)
+  SurfDescriptorExtractor extractor;
+
+  Mat descriptors_object, descriptors_scene;
+
+  extractor.compute( img_object, keypoints_object, descriptors_object );
+  extractor.compute( img_scene, keypoints_scene, descriptors_scene );
+
+  //-- Step 3: Matching descriptor vectors using FLANN matcher
+  FlannBasedMatcher matcher;
+  std::vector< DMatch > matches;
+  matcher.match( descriptors_object, descriptors_scene, matches );
+
+  double max_dist = 0; double min_dist = 100;
+
+  //-- Quick calculation of max and min distances between keypoints
+  for( int i = 0; i < descriptors_object.rows; i++ )
+  { double dist = matches[i].distance;
+    if( dist < min_dist ) min_dist = dist;
+    if( dist > max_dist ) max_dist = dist;
+  }
+
+  printf("-- Max dist : %f \n", max_dist );
+  printf("-- Min dist : %f \n", min_dist );
+
+  //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+  std::vector< DMatch > good_matches;
+
+  for( int i = 0; i < descriptors_object.rows; i++ )
+  { if( matches[i].distance < 3*min_dist )
+    { good_matches.push_back( matches[i]); }
+  }
+
+  Mat img_matches;
+  drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
+               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+
+  //-- Localize the object from img_1 in img_2
+  std::vector<Point2f> obj;
+  std::vector<Point2f> scene;
+
+  for( int i = 0; i < good_matches.size(); i++ )
+  {
+    //-- Get the keypoints from the good matches
+    obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+    scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+  }
+
+  Mat H = findHomography( obj, scene, CV_RANSAC );
+
+  //-- Get the corners from the image_1 ( the object to be "detected" )
+  std::vector<Point2f> obj_corners(4);
+  obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
+  obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+  std::vector<Point2f> scene_corners(4);
+
+  perspectiveTransform( obj_corners, scene_corners, H);
+
+
+  //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+  line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+  line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+  line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+  line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+
+  //-- Show detected matches
+  imshow( "Good Matches & Object detection", img_matches );
+
+  waitKey(0);
+
+  return 0;
 }
 
-int main(int argc, char** argv)
-{
-    if(argc != 3)
-    {
-        help();
-        return -1;
-    }
-
-    Mat img1, img2;
-    img1 = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-    img2 = cv::imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
-    if(img1.empty() || img2.empty())
-    {
-        printf("Can't read one of the images\n");
-        return -1;
-    }
-
-    printf("images loaded\n");
-
-    // detecting keypoints
-    printf("Detecting keypoints...\n");
-    FastFeatureDetector detector(50);
-    vector<KeyPoint> keypoints1, keypoints2;
-    detector.detect(img1, keypoints1);
-    detector.detect(img2, keypoints2);
-
-    // computing descriptors
-    printf("Computing descriptors...\n");
-    SiftDescriptorExtractor extractor;
-    Mat descriptors1, descriptors2;
-    extractor.compute(img1, keypoints1, descriptors1);
-    extractor.compute(img2, keypoints2, descriptors2);
-
-    // matching descriptors
-    printf("Matching descriptors...\n");
-    BFMatcher matcher(NORM_L2);
-    vector<DMatch> matches;
-    matcher.match(descriptors1, descriptors2, matches);
-    std::sort(matches.begin(), matches.end());
-
-    // drawing the results
-    printf("Drawing the results...\n");
-    namedWindow("matches", 1);
-    Mat img_matches;
-
-    size_t chunk_size = 10;
-    for( int i = 0; i < matches.size() ; i += chunk_size ) {
-        vector<DMatch> top_matches(matches.begin() + i, matches.begin() + min<int>(i + chunk_size, matches.size()));
-        drawMatches(img1, keypoints1, img2, keypoints2, top_matches, img_matches);
-        imshow("matches", img_matches);
-        waitKey();
-    }
-
-    return 0;
-}
+/**
+ * @function readme
+ */
+void readme()
+{ std::cout << " Usage: ./SURF_Homography <img1> <img2>" << std::endl; }
