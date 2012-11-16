861b67a2640ec2feb10c3f1aa9afd5ed28c6f434
Alexei Mozhaev <lxmzhv@gmail.com>
2012-11-16 15:35:06 +0400

    Added AllSampleGenerator

--- brief_match/include/photo_match.hpp
+++ brief_match/include/photo_match.hpp
@@ -21,40 +21,90 @@ class VerbosTimer
 };
 
 template<class T, int N>
-class RandomSampleGenerator
+class SampleGenerator
 {
   public:
-    RandomSampleGenerator(const T& min_val, const T& max_val, bool _different = false):
-      minValue(min_val), maxValue(max_val), different(_different) {
+    SampleGenerator(const T& min_val, const T& max_val):
+      minValue(min_val), maxValue(max_val), _empty(true) {
+    }
+
+    virtual bool operator ++ () = 0;
+    const T& operator [] (int n) const { return values[n]; }
+    bool empty() const { return _empty; }
+
+  protected:
+    const T minValue, maxValue;
+    T values[N];
+    bool _empty;
+};
+
+template<class T, int N, bool different>
+class RandomSampleGenerator: public SampleGenerator<T, N>
+{
+  typedef SampleGenerator<T, N> TParent;
+
+  public:
+    RandomSampleGenerator(const T& min_val, const T& max_val):
+      TParent(min_val, max_val) {
         ++(*this);
     }
 
-    const RandomSampleGenerator& operator ++ () {
-      T v = minValue;
+    virtual bool operator ++ () {
+      T v = TParent::minValue;
       bool found = false;
 
       for (int j = 0, i = 0; i < N;) {
-        v = (T)(minValue + rand()*(maxValue*1.0/RAND_MAX));
+        // TODO optimize for integers!
+        v = (T)(TParent::minValue + rand()*(TParent::maxValue*1.0/RAND_MAX));
         found = false;
         if (different)
           for (j = i-1; j>=0; --j)
-            if (v == values[j]) {
+            if (v == TParent::values[j]) {
               found = true;
               break;
             }
         if (!found)
-          values[i++] = v;
+          TParent::values[i++] = v;
       }
+      TParent::_empty = false;
+      return true;
+    }
+};
 
-      return *this;
+template<class T, int N>
+class AllSampleGenerator: public SampleGenerator<T, N>
+{
+  typedef SampleGenerator<T, N> TParent;
+
+  public:
+    AllSampleGenerator(const T& min_val, const T& max_val):
+        TParent(min_val, max_val) {
+      Init();
     }
 
-    const T& operator [] (int n) { return values[n]; }
+    void Init() {
+      if (TParent::maxValue - TParent::minValue >= N-1) {
+        for (int i = 0; i < N; ++i)
+          TParent::values[i] = TParent::minValue + i;
+        TParent::_empty = false;
+      } else
+        TParent::_empty = true;
+    }
 
-  private:
-    const T minValue, maxValue;
-    T values[N];
-    bool different;
+    virtual bool operator ++ () {
+      if (TParent::_empty)
+        return false;
+      for (int i = N-1; i >= 0; --i) {
+        if (TParent::values[i] <= TParent::maxValue - N + i) {
+          T value = ++TParent::values[i];
+          for (int j = i+1; j < N; ++j)
+            TParent::values[j] = ++value;
+          return true;
+        }
+      }
+      TParent::_empty = true;
+      return false;
+    }
 };
 
 extern bool matchImagesAndPutLabel(const cv::Mat& img1,
